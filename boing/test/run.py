20115eaaab1a6d8be1cff55e40dde02c824a1291
Paolo Olivo <paolo.olivo@inria.fr>
2012-03-30 14:35:04 +0200

    Added setup.py to enable boing package installation.
    --HG--
    rename : bin/run.py => boing/run.py

--- boing/test/run.py
+++ boing/test/run.py
@@ -8,58 +8,55 @@
 # See the file LICENSE for information on usage and redistribution of
 # this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
-import getopt
-import sys
 import unittest
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "h", ('help', ))
-except getopt.GetoptError as err:
-    print(str(err))
-    print("usage: %s [<module>...]"%sys.argv[0])
-    print("       %s [-h, --help]"%(" "*len(sys.argv[0])))
-    sys.exit(2)
+def additional_tests(modules=[]):
 
-for o, a in opts:
-    if o in ("-h", "--help"):
-        print("usage: %s [<module>...]"%sys.argv[0])
-        print("       %s [-h, --help]"%(" "*len(sys.argv[0])))
-        print("""
+    tests = []
+    if not modules or 'dns_sd' in modules:
+        from boing.test.net import test_dns_sd
+        tests += test_dns_sd.suite()
+    if not modules or 'core' in modules:
+        from boing.test.core import test_ReactiveObject
+        tests += test_ReactiveObject.suite()
+    if not modules or 'json' in modules:
+        from boing.test.net import test_json
+        tests += test_json.suite()
+    if not modules or 'osc' in modules:
+        from boing.test.net import test_osc
+        tests += test_osc.suite()
+    if not modules or 'slip' in modules:
+        from boing.test.net import test_slip
+        tests += test_slip.suite()
+    if not modules or 'tcp' in modules:
+        from boing.test.net import test_tcp
+        tests += test_tcp.suite()
+    if not modules or 'udp' in modules:
+        from boing.test.net import test_udp
+        tests += test_udp.suite()
+    if not modules or 'utils' in modules:
+        from boing.test.utils import test_QPath, test_display
+        tests += test_QPath.suite()
+        tests += test_display.suite()
+
+    return unittest.TestSuite(tests)
+
+
+if __name__=="__main__":
+    import argparse
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""
 If no module is specified, all modules are tested; otherwise only the
-specified modules will be executed. 
- 
+specified modules will be executed.
+
 Available modules: 
- dns_sd, core, json, osc, slip, tcp, udp, utils
-""")
-        sys.exit(0)
+ dns_sd, core, json, osc, slip, tcp, udp, utils""")
 
-list = []
-if len(sys.argv)==1 or 'dns_sd' in sys.argv:
-    from net import test_dns_sd
-    list += test_dns_sd.suite()
-if len(sys.argv)==1 or 'core' in sys.argv:
-    from core import test_ReactiveObject
-    list += test_ReactiveObject.suite()
-if len(sys.argv)==1 or 'json' in sys.argv:
-    from net import test_json
-    list += test_json.suite()
-if len(sys.argv)==1 or 'osc' in sys.argv:
-    from net import test_osc
-    list += test_osc.suite()
-if len(sys.argv)==1 or 'slip' in sys.argv:
-    from net import test_slip
-    list += test_slip.suite()
-if len(sys.argv)==1 or 'tcp' in sys.argv:
-    from net import test_tcp
-    list += test_tcp.suite()
-if len(sys.argv)==1 or 'udp' in sys.argv:
-    from net import test_udp
-    list += test_udp.suite()
-if len(sys.argv)==1 or 'utils' in sys.argv:
-    from utils import test_QPath, test_display
-    list += test_QPath.suite()
-    list += test_display.suite()
-
-alltests = unittest.TestSuite(list)
-runner = unittest.TextTestRunner()
-runner.run(alltests)
+    parser.add_argument("module", metavar="MODULE", nargs="*", default=[],
+                        help="module to be tested")
+    args = parser.parse_args()
+    runner = unittest.TextTestRunner()
+    runner.run(additional_tests(args.module))
---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# setup.py -
+#
+# Author: Paolo Olivo (paolo.olivo@inria.fr)
+#
+# See the file LICENSE for information on usage and redistribution of
+# this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "boing",
+    version = "0.2.0",
+    packages = find_packages(), 
+    entry_points = {'console_scripts': ["boing = boing.run"]},
+    test_suite = "boing.test.run",
+
+    author = "Paolo Olivo",
+    author_email = "paolo.olivo@inria.fr",
+    description = """
+On demand producer-consumer architecture for connecting different
+sources (e.g. devices, sockets, files, etc.) to different outputs, while
+applying flexible data processing.""",
+    license = "GPLv2", )
